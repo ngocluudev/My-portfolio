41f15d5cec69bb5fa98e6964f559ffb9d2eff131
ngoctechno <luuxuanngoc2312@gmail.com>
2022-09-24 12:32:13 +0700

    commit change

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -5,15 +5,6 @@ import PortfolioImg from '../images/portfolioImg.png';
 const projects = [
   {
     id: 1,
-    name: 'Cryptocurrency App',
-    desc:
-      'A website that help you to know more about cryptocurrencies. This website was made from React, Redux, Typescript and more',
-    img: CryptoAppImg,
-    link: 'https://ngocluu-crypto-app.vercel.app/',
-    source: 'https://github.com/ngocluudev/Crypto-App',
-  },
-  {
-    id: 2,
     name: 'Cocktail Search App',
     desc:
       'A website to search your favorite cocktail.React js and fetch API is used in this project. ',
@@ -22,13 +13,22 @@ const projects = [
     source: 'https://github.com/ngocluudev/react-project',
   },
   {
-    id: 3,
+    id: 2,
     name: 'My Portfolio',
     desc:
       'My personal portfolio website using React js, Styled-components, swiper and responsive design.  ',
     img: PortfolioImg,
     link: 'https://ngocluu-portfolio.vercel.app/',
     source: 'https://github.com/ngocluudev/My-portfolio',
+  },
+  {
+    id: 3,
+    name: 'Cryptocurrency App',
+    desc:
+      'A website that help you to know more about cryptocurrencies. This website was made from React, Redux, Typescript, antd and more',
+    img: CryptoAppImg,
+    link: 'https://ngocluu-crypto-app.vercel.app/',
+    source: 'https://github.com/ngocluudev/Crypto-App',
   },
 ];
 
